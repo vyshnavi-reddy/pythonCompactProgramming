046e9ad7fde74546d3f7a7a3efcff101b83d5e2c
Vyshnavi Reddy <vyshnavireddy@Vyshnavis-MacBook-Air.local>
2026-09-16 11:58:04 +0530

    Complete Week 1 Python tasks

---
+++ main.py
@@ -0,0 +1,8 @@
+from math import pi
+
+if __name__ == "__main__":
+    r = float(input("Input the radius of the circle: "))
+    area = pi * r ** 2
+
+    print("The area of the circle with radius", r, "is:", area)
+
